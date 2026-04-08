8c5ffadc4c06afa46ab5340a9968dc4b44833bd1
andjelko-A <analeksandar475@gmail.com>
2026-04-08 21:01:15 +0200

    Timeout constants changed according to competition

--- test3/include/timer.h
+++ test3/include/timer.h
@@ -6,8 +6,8 @@
 
 #define TIMER_TAG "TIMER.H"
 
-#define DISABLED_TIME_uS 5000000
-#define RUN_TIME_uS 20500000
+#define DISABLED_TIME_uS 86000000
+#define RUN_TIME_uS 13000000
 
 volatile bool end_flag = false;
 bool start_flag = false;
